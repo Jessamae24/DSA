212d856dbccadaf976d4054e2600881857dd807d
Jessamae24 <jessawabinga@gmail.com>
2016-10-05 11:56:26 +0800

    Add files via upload

---
+++ SumAveMax.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.Random;
+public class SumAveMax 
+{
+	static Scanner c = new Scanner (System.in);
+	public static void main (String [] args)
+	{
+		
+		int total = 0, ave = 0, large = 0;
+		System.out.println("Please enter array size: ");
+		int n = c.nextInt();
+		int[] numbers = new int [n];
+				
+				for (int i = 0; i < numbers.length; i++)
+				{
+					numbers[i] = (int) (Math.random()*1000 + 1 );
+				}
+		System.out.println("Numbers Generated: "+ Arrays.toString(numbers));
+		
+		for (int j=0; j<numbers.length; j++)
+		{
+			total = total + numbers [j];
+		}
+		System.out.println ("Total = " +total);
+		ave = total/numbers.length;
+		System.out.println ("Average = " +ave);
+		
+		for (int i=0; i<numbers.length; i++)
+		{
+			if (numbers[i] > large)
+				large = numbers[i];
+		}
+		System.out.println("Largest number is: " +large);
+	}
+
+}
